45bf71fb6a3446a0f0bfb95a6adbbe74c030a417
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-05-18 18:37:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify'
 
 const App = () => {
 
-  const url = 'http://localhost:4000'
+  const url = 'https://shop-mash.onrender.com'
 
   return (
     <div>
