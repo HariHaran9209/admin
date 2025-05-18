3a753cab4f6efaea559a0bd792839f619b305a63
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-05-18 18:12:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 5173, // or leave it out; Render sets it via $PORT
+    host: true,
+    allowedHosts: ['shop-mash.onrender.com'],
+  },
+  build: {
+    outDir: 'dist',
+  }
 })
